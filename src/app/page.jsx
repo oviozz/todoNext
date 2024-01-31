f060007a4151dd08db9a26ba6743f4774cffe3c7
prajwal <42685801+oviozz@users.noreply.github.com>
2024-01-30 16:15:52 -0800

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import TopicList from "@/components/TopicList";
 const getTopics = async () => {
     try{
 
-        const res = await fetch("http://localhost:3000/api/topics",{
+        const res = await fetch("/api/topics",{
             cache: "no-store"
         })
 
