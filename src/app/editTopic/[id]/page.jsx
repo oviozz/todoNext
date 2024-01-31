0762d8b2ff912a817f9a62e7dd9f01fe47fe6049
prajwal <42685801+oviozz@users.noreply.github.com>
2024-01-30 19:13:13 -0800

    Update page.jsx

--- src/app/editTopic/[id]/page.jsx
+++ src/app/editTopic/[id]/page.jsx
@@ -6,7 +6,7 @@ import FormButton from "@/components/FormButton";
 
 const getTopicById = async (id) => {
 
-    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+    const res = await fetch(`https://todo-next-seven-iota.vercel.app/api/topics/${id}`, {
         cache: "no-store"
     })
     if (!res.ok){
@@ -17,7 +17,7 @@ const getTopicById = async (id) => {
 }
 
 const updateTopicByID = async (id, formData) => {
-    const res = await fetch(`http://localhost:3000/api/topics/${id}`,{
+    const res = await fetch(`https://todo-next-seven-iota.vercel.app/api/topics/${id}`,{
         cache: "no-cache",
         method: 'PUT',
         headers: {
